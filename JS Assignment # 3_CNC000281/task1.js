5b30ca1ca5e9fb50fce9d5c3fb01959cc11a1d85
srahmed <srehanpk@gmail.com>
2019-12-25 22:52:49 +0500

    JS assignment 3

---
+++ JS Assignment # 3_CNC000281/task1.js
@@ -0,0 +1,19 @@
+
+
+var a=["SSC","HSC","BCS","BS","B.COM","MS","M.Phil","PhD"]
+
+console.log(a);
+
+
+
+for(var x=0; x<=7; x++) {
+
+if (x==0) {document.write("1) " + a[0]);}
+else if (x==1) {document.write("<br>2) " + a[1]);}
+else if (x==2) {document.write("<br>3) " + a[2]);}
+else if (x==3) {document.write("<br>4) " + a[3]);}
+else if (x==4) {document.write("<br>5) " + a[4]);}
+else if (x==5) {document.write("<br>6) " + a[5]);}
+else if (x==6) {document.write("<br>7) " + a[6]);}
+else if (x==7) {document.write("<br>8) " + a[7]);}
+}
