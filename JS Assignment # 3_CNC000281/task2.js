ef8a609a1e4c792808067c259dae4dd3f6ac0613
srahmed <srehanpk@gmail.com>
2020-01-06 15:41:05 +0500

    task 2

---
+++ JS Assignment # 3_CNC000281/task2.js
@@ -0,0 +1,26 @@
+var stu = ["Ahmed", "Salman", "Imran"];
+
+
+
+var score=[320,230,480];
+
+var ts=500;
+
+
+var s1= (score[0]/ts)*100;
+var s2= (score[1]/ts)*100;
+var s3= (score[2]/ts)*100;
+
+
+document.write("Score of "+ stu[0]+" is "+score[0]+". Percentage: "+s1+"%");
+
+document.write("<br><br>Score of "+ stu[1]+" is "+score[1]+". Percentage: "+s2+"%");
+
+document.write("<br><br>Score of "+ stu[2]+" is "+score[2]+". Percentage: "+s3+"%");
+
+
+console.log("Score of "+ stu[0]+" is "+score[0]+". Percentage: "+s1+"%");
+
+console.log("Score of "+ stu[1]+" is "+score[1]+". Percentage: "+s2+"%");
+
+console.log("Score of "+ stu[2]+" is "+score[2]+". Percentage: "+s3+"%");
