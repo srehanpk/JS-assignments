3869c352229710fa707a9233493bfa9b25521e0f
srahmed <srehanpk@gmail.com>
2020-01-06 18:32:55 +0500

    task 4

---
+++ JS Assignment # 3_CNC000281/task4.js
@@ -0,0 +1,11 @@
+var cityList=["Karachi ", "Lahore ", "Islamabad ", "Quetta ", "Peshawar "];
+
+document.write("<h2>Cities list:</h2>" + cityList);
+
+
+
+var select = cityList.slice(1,4);
+
+
+document.write("<br><br><h3>Selected cities list from index 1 to 3:</h3>"+select);
+
