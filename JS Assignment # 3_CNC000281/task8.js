d96987dbbe8d98ff9359a56b4390d500eab91169
srahmed <srehanpk@gmail.com>
2020-01-08 16:02:07 +0500

    task 8

---
+++ JS Assignment # 3_CNC000281/task8.js
@@ -0,0 +1,88 @@
+
+
+//Counting:
+var counting = [];
+
+for (var i=1; i<=15; i++) {
+
+	counting.push(i);
+	}
+
+document.write("Counting:<br> "+counting);
+
+
+//Reverse counting:
+
+var rCounting = [];
+
+for (var i=10; i>=1; i--) {
+
+	rCounting.push(i);
+	}
+
+document.write("<br><br>Reverse Counting:<br> "+rCounting);
+
+
+
+//Even number:
+
+var even = [];
+
+for (var i = 0; i <= 10; i++) {
+
+
+	even.push(i*2);
+	}
+
+document.write("<br><br>Even Number:<br> "+even);
+
+
+
+//Odd number:
+
+var odd = [];
+
+for (var i = 0; i < 10; i++) {
+
+
+	odd.push((i*2)+1);
+	}
+
+document.write("<br><br>odd Number:<br> "+odd);
+
+
+
+//Series:
+
+var ser = [];
+
+for (var i = 1; i <= 10; i++) {
+
+
+	ser.push((i*2)*1000+"k");
+	}
+
+document.write("<br><br>Series:<br> "+ser);
+
+
+
+//Series:
+
+var ser1 = [];
+
+for (var i = 1; i <= 10; i++) {
+
+
+	ser1.push((i*2)+"k");
+	}
+
+document.write("<br><br>Series:<br> "+ser1);
+
+
+
+
+
+
+
+
+
