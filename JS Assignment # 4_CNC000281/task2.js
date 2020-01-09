38faa2c05970bce47482f032e9f3a31147660e66
srahmed <srehanpk@gmail.com>
2020-01-09 19:50:22 +0500

    task 2

---
+++ JS Assignment # 4_CNC000281/task2.js
@@ -0,0 +1,19 @@
+
+
+var num = prompt("Input number");
+
+document.write("<b>Value is : </b>"+num);
+document.write("<br><b>Type is:</b> string");
+console.log(num);
+
+var add = num + 2;
+
+console.log(add);
+
+num = Number(num);
+
+var add1 = num + 2;
+
+document.write("<br><br><b>Value is : </b>"+num);
+document.write("<br><b>Type is:</b> Number");
+console.log(add1);
