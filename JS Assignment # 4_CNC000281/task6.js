321edfcc14bedac5e80b18272179cc0557978876
srahmed <srehanpk@gmail.com>
2020-01-10 17:21:47 +0500

    task 6

---
+++ JS Assignment # 4_CNC000281/task6.js
@@ -0,0 +1,21 @@
+
+for (var i =0; i<6; i++) {
+
+
+var random = Math.random();
+
+var num = (random * 6) + 1;
+
+var dice = Math.floor(num);
+
+
+	document.write("<br><br>Random dice value is: "+ dice);
+
+}
+
+
+
+
+
+
+		
