4e2eadd6616247f83dbbb6b9f536c45d1f50c575
srahmed <srehanpk@gmail.com>
2020-01-10 16:13:34 +0500

    task 5

---
+++ JS Assignment # 4_CNC000281/task5.js
@@ -0,0 +1,26 @@
+var input = prompt("Enter Number");
+
+    document.write("User input is: "+ input);
+
+var round = Math.round(input);
+
+    document.write("<br><br> Round off value is: "+ round);
+
+
+var floor = Math.floor(input);
+
+	document.write("<br><br> Floor value is: "+ floor);
+
+
+var ceil = Math.ceil(input);
+
+	document.write("<br><br> Ceil value is: "+ ceil);
+
+
+
+
+
+
+
+
+		
