fe089a5f61fbf02870c5abbf45806d7ac257a0ed
srahmed <srehanpk@gmail.com>
2020-01-11 21:01:55 +0500

    task 9

---
+++ JS Assignment # 4_CNC000281/task9.js
@@ -0,0 +1,17 @@
+
+
+var a = "<p><strong><em>Only print this</em></strong></p>";
+
+
+document.write("Before: " + a);
+console.log(a);
+
+
+var b = a.slice(15,30);
+
+document.write("After:<br> " + b);
+console.log(b);
+
+
+
+
