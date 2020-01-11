7fb0e7eb90e5b05eaf88bc317de02599fb7561b5
srahmed <srehanpk@gmail.com>
2020-01-11 19:32:17 +0500

    task 8

---
+++ JS Assignment # 4_CNC000281/task8.js
@@ -0,0 +1,141 @@
+
+var num = ["Zero"," One"," Two"," Three"," Four"," Five"," Six"," Seven"," Eight"," Nine"," Ten"," Eleven"," Twelve"," Thirteen"," Forteen"," Fifteen"," Sixteen"," Seventeen"," Eighteen"," Nineteen"," Twenty","","","","","","","","",""," Thirty","","","","","","","","",""," Fourty","","","","","","","","",""," Fifty","","","","","","","","",""," Sixty","","","","","","","","",""," Seventy","","","","","","","","",""," Eighty","","","","","","","","",""," Ninety","","","","","","","","",""," One Hundred"];
+
+var a = prompt("Input number\n 0-200");
+
+document.write("Input: "+ a)     
+
+
+
+	if (a <= 20 ) {
+    document.write("<br><br> Output: "+num[a]);}
+
+else if ( a > 20 && a < 30){
+	document.write("<br><br> Output: "+num[20] + num[a[1]]);}
+
+else if ( a == 30){
+	document.write("<br><br> Output: "+num[30]);}
+
+	else if ( a > 30 && a < 40){
+	document.write("<br><br> Output: "+num[30] + num[a[1]]);}
+
+else if ( a == 40 ){
+	document.write("<br><br> Output: "+num[40]);}
+
+	else if ( a > 40 && a < 50){
+	document.write("<br><br> Output: "+num[40] + num[a[1]]);}
+
+else if ( a == 50){
+	document.write("<br><br> Output: "+num[50]);}
+
+	else if ( a > 50 && a < 60){
+	document.write("<br><br> Output: "+num[50] + num[a[1]]);}
+
+else if ( a == 60){
+	document.write("<br><br> Output: "+num[60] );}
+
+	else if ( a > 60 && a < 70){
+	document.write("<br><br> Output: "+num[60] + num[a[1]]);}
+
+else if ( a == 70){
+	document.write("<br><br> Output: "+num[70]);}
+
+	else if ( a > 70 && a < 80){
+	document.write("<br><br> Output: "+num[70] + num[a[1]]);}
+
+else if ( a == 80){
+	document.write("<br><br> Output: "+num[80]);}
+
+    else if ( a > 80 && a < 90){
+	document.write("<br><br> Output: "+num[80] + num[a[1]]);}
+
+else if ( a == 90){
+	document.write("<br><br> Output: "+num[90]);}
+
+	else if ( a > 90 && a < 100){
+	document.write("<br><br> Output: "+num[90] + num[a[1]]);}
+
+else if ( a == 100){
+	document.write("<br><br> Output: "+num[100]);}
+
+	else if ( a > 100 && a < 110){
+	document.write("<br><br> Output: "+num[100] + num[a[2]]);}
+
+else if ( a == 110){
+	document.write("<br><br> Output: "+num[100] + num[10]);}
+
+
+	else if ( a > 110 && a < 120){
+	document.write("<br><br> Output: "+num[100] + num[a[1]+a[2]]);}
+
+else if ( a == 120){
+	document.write("<br><br> Output: "+num[100] + num[20]);}
+
+
+	else if ( a > 120 && a < 130){
+	document.write("<br><br> Output: "+num[100] + num[20]+num[a[2]]);}
+
+else if ( a == 130){
+	document.write("<br><br> Output: "+num[100] + num[30]);}
+
+	 else if ( a > 130 && a < 140){
+	document.write("<br><br> Output: "+num[100] + num[30]+num[a[2]]);}
+
+else if ( a == 140){
+	document.write("<br><br> Output: "+num[100] + num[40]);}
+
+
+ 	else if ( a > 140 && a < 150){
+	document.write("<br><br> Output: "+num[100] + num[40]+num[a[2]]);}
+
+else if ( a == 150){
+	document.write("<br><br> Output: "+num[100] + num[50]);}
+
+
+else if ( a > 150 && a < 160){
+	document.write("<br><br> Output: "+"<br><br> Output: "+num[100] + num[50]+num[a[2]]);}
+
+else if ( a == 160){
+	document.write("<br><br> Output: "+num[100] + num[60]);}
+
+
+	else if ( a > 160 && a < 170){
+	document.write("<br><br> Output: "+num[100] + num[60]+num[a[2]]);}
+
+else if ( a == 170){
+	document.write("<br><br> Output: "+num[100] + num[70]);}
+
+
+	else if ( a > 170 && a < 180){
+	document.write("<br><br> Output: "+num[100] + num[70]+num[a[2]]);}
+
+else if ( a == 180){
+	document.write("<br><br> Output: "+num[100] + num[80]);}
+
+
+	else if ( a > 180 && a < 190){
+	document.write("<br><br> Output: "+num[100] + num[80]+num[a[2]]);}
+
+else if ( a == 190){
+	document.write("<br><br> Output: "+num[100] + num[90]);}
+
+
+	else if ( a > 190 && a < 200){
+	document.write("<br><br> Output: "+num[100] + num[90]+num[a[2]]);}
+
+else if ( a == 200){
+	document.write("<br><br> Output: Two Hundred");}
+
+
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+		
