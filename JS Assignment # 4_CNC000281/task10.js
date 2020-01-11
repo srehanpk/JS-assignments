d01e3d2406839d4e3d9d417af297bfd43f1a52c2
srahmed <srehanpk@gmail.com>
2020-01-11 21:32:04 +0500

    task 10

---
+++ JS Assignment # 4_CNC000281/task10.js
@@ -0,0 +1,23 @@
+
+
+var input = [16.765,12.37982,-9.3079499];
+
+
+
+
+
+for (var i = 0; i < input.length; i++) {
+
+	if (i == 0) {document.write("First Input: "+ input[i]);
+
+		document.write("<br> First Output: "+ input[i].toFixed(2));}
+
+			if (i == 1) {document.write("<br><br> Second Input: "+ input[i]);
+
+				document.write("<br> Second Output: "+ input[i].toFixed(2));}
+		
+					if (i == 2) {document.write("<br><br> Third Input: "+ input[i]);
+
+						document.write("<br> Third Output: "+ input[i].toFixed(4));}
+
+} 
