bb7c0446048d7c68ca62b39b592d8d1da7b1b06d
srahmed <srehanpk@gmail.com>
2020-01-12 16:35:56 +0500

    task 15

---
+++ JS Assignment # 4_CNC000281/task15.js
@@ -0,0 +1,15 @@
+var age = prompt("What is your age?");
+
+var d = new Date();
+
+	document.write("<b><br><br> Your age is : </b>"+ age);
+
+var fy = d.getFullYear();
+
+var by = fy - age 
+
+	document.write("<br><br><b> Your birth year is : </b>" + by );
+
+
+
+
