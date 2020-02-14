abb4a37a9bd92e83003dbf2634a540208a7ca8e4
srahmed <srehanpk@gmail.com>
2020-02-14 15:32:33 +0500

    task 14 update

--- JS Assignment # 4_CNC000281/task14.js
+++ JS Assignment # 4_CNC000281/task14.js
@@ -1,9 +1,14 @@
 
 var d = new Date();
+var t1 = d.getTime();
 
 document.write("<b>Current Date : </b>"+ d);
 
-d.setHours(14);
+var sec = 1000*60*60;
+
+var t2 = t1-sec;
+
+d.setTime(t2);
 
 document.write("<br><br><b> One hour ago, it was : </b>" + d )
 
