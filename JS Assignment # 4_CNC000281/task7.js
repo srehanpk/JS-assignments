c9011648124f129b943a68dd9a5cd34027479716
srahmed <srehanpk@gmail.com>
2020-02-15 22:18:27 +0500

    task 7

---
+++ JS Assignment # 4_CNC000281/task7.js
@@ -0,0 +1,41 @@
+
+
+var text1 = prompt("Write a sentance or paragraph.","The quick brown fox jumped over the laizy dog.");
+
+var ip = prompt("Write a word which you want to find. ","The")
+
+document.write("<h2>The Main Text:</h2> "+"<b>"+ text1+"</b>");
+
+var text = text1.toLowerCase();
+
+var len = text.length;
+
+var count1 = [];
+
+
+
+
+for (var i = 0; i < len; i++){
+
+	find = text.indexOf(ip.toLowerCase(),i);
+
+if (find!==-1 && find!==count1[count1.length-1]){
+	count1.push(find);
+}
+
+
+}
+
+var count = count1.length;
+
+console.log(count);
+
+document.write("<h3> There are "+count+" accurence of word "+ip+"</h3>")
+
+
+
+
+
+
+
+		
